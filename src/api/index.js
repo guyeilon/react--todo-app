b9e586e4da1d4f6bdb57b32b344414f19402af8f
guyeilon <guyeilon84@gmail.com>
2022-01-27 00:10:12 +0200

    react app

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 // export const url = 'http://localhost:5000/api';
-export const url = ' https://guy-node-todo-app.herokuapp.com/api';
+export const url = "https://guy-node-todo-app.herokuapp.com/api";"
 
 export const setHeaders = () => {
   const header = {
