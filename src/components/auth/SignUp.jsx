fa072dde65c80dbe761667c752c0a7fa98694890
guyeilon <guyeilon84@gmail.com>
2022-03-02 23:14:22 +0200

    fix

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -8,89 +8,91 @@ import { makeStyles } from '@material-ui/styles';
 import { signUp } from '../../store/actions/authActions';
 
 const useStyles = makeStyles({
-  formStyle: {
-    margin: '0px auto',
-    padding: '30px',
-    borderRadius: '9px',
-    boxShadow: '0px 0px 12px -3px #000000',
-  },
-  spacing: {
-    '&&': {
-      marginTop: '20px',
-    },
-  },
+	formStyle: {
+		margin: '0px auto',
+		padding: '30px',
+		borderRadius: '9px',
+		boxShadow: '0px 0px 12px -3px #000000',
+	},
+	spacing: {
+		'&&': {
+			marginTop: '20px',
+		},
+	},
 });
 
 const SingUp = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const state = useSelector(state => state);
-  console.log(state);
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
+	const classes = useStyles();
+	const dispatch = useDispatch();
+	const state = useSelector(state => state);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    dispatch(signUp(user));
-    setUser({
-      name: '',
-      email: '',
-      password: '',
-    });
-  };
-  if (state.auth._id) return <Navigate to='/' />;
+	const [user, setUser] = useState({
+		name: '',
+		email: '',
+		password: '',
+	});
 
-  return (
-    <>
-      <form
-        className={classes.formStyle}
-        noValidate
-        autoComplete='off'
-        onSubmit={handleSubmit}
-      >
-        <Typography variant='h5'>singUp;</Typography>
-        <TextField
-          className={classes.spacing}
-          id='enter-name'
-          label='enterName'
-          variant='outlined'
-          fullWidth
-          value={user.name}
-          onChange={e => setUser({ ...user, name: e.target.value })}
-        />
-        <TextField
-          className={classes.spacing}
-          id='enter-email'
-          label='enterEmail'
-          variant='outlined'
-          fullWidth
-          value={user.email}
-          onChange={e => setUser({ ...user, email: e.target.value })}
-        />
-        <TextField
-          className={classes.spacing}
-          id='enter-password'
-          type='password'
-          label='enterPassword'
-          variant='outlined'
-          fullWidth
-          value={user.password}
-          onChange={e => setUser({ ...user, password: e.target.value })}
-        />
-        <Button
-          className={classes.spacing}
-          color='primary'
-          variant='contained'
-          type='submit'
-        >
-          signup
-        </Button>
-      </form>
-    </>
-  );
+	const handleSubmit = e => {
+		e.preventDefault();
+		dispatch(signUp(user));
+		setUser({
+			name: '',
+			email: '',
+			password: '',
+		});
+	};
+	if (state.auth._id) return <Navigate to='/' />;
+
+	return (
+		<>
+			<form
+				className={classes.formStyle}
+				noValidate
+				autoComplete='off'
+				onSubmit={handleSubmit}
+			>
+				<Typography variant='h5'>singUp;</Typography>
+				<TextField
+					className={classes.spacing}
+					id='enter-name'
+					label='enterName'
+					variant='outlined'
+					fullWidth
+					value={user.name}
+					onChange={e => setUser({ ...user, name: e.target.value })}
+				/>
+				<TextField
+					className={classes.spacing}
+					id='enter-email'
+					label='enterEmail'
+					variant='outlined'
+					fullWidth
+					value={user.email}
+					onChange={e => setUser({ ...user, email: e.target.value })}
+				/>
+				<TextField
+					className={classes.spacing}
+					id='enter-password'
+					type='password'
+					label='enterPassword'
+					variant='outlined'
+					fullWidth
+					value={user.password}
+					onChange={e =>
+						setUser({ ...user, password: e.target.value })
+					}
+				/>
+				<Button
+					className={classes.spacing}
+					color='primary'
+					variant='contained'
+					type='submit'
+				>
+					signup
+				</Button>
+			</form>
+		</>
+	);
 };
 
 export default SingUp;
